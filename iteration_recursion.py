65a4b56ac9693935b537f6e0595aa28ffdf9d9ac
Angelica <Angelica137@users.noreply.github.com>
2020-06-18 20:05:01 +0100

    first commit

---
+++ iteration_recursion.py
@@ -0,0 +1,23 @@
+x = range(4)
+print(x)
+for i in x:
+    print(i)
+
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    number = 1
+    if exp == 0:
+        return number
+    else:
+        for i in range(exp):
+            number *= base
+    return number
+
+
+print(iterPower(6.62, 7))
