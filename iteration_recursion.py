a4e9f6eadf639119a381152bcc645d1cdfbf87e4
Angelica <Angelica137@users.noreply.github.com>
2020-06-18 20:45:25 +0100

    add recursive function, test for 0 exp fail

--- iteration_recursion.py
+++ iteration_recursion.py
@@ -14,4 +14,14 @@ def iterPower(base, exp):
     return number
 
 
-print(iterPower(6.62, 7))
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    if(exp == 1):
+        return base
+    else:
+        return base*recurPower(base, exp - 1)
